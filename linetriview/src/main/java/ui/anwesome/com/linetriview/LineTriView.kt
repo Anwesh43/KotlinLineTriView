787f47c6a3dae57c45b71ea085336cb582c4942d
Anwesh43 <anweshthecool0@gmail.com>
2018-03-12 08:36:41 +0530

    adding LineTriView class

--- linetriview/src/main/java/ui/anwesome/com/linetriview/LineTriView.kt
+++ linetriview/src/main/java/ui/anwesome/com/linetriview/LineTriView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.linetriview
 /**
  * Created by anweshmishra on 12/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class LineTriView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
